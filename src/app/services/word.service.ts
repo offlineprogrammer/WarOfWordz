c9df17d1b1734b38e64d99f74d3967f31564eddc
offlineprogrammer <mmalaka@gmail.com>
2018-08-05 23:13:26 -0700

    service

--- src/app/services/word.service.ts
+++ src/app/services/word.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'X-Mashape-Key':  'jivIiF0vmwmshcMwZNlOi9My5iPSp1OP1pYjsnBImh0TGx2vdI',
+    'Accept': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class WordService {
 
-  constructor() { }
+  wordAPIUrl: string = 'https://wordsapiv1.p.mashape.com/words/';
+
+ 
+
+  constructor(private http: HttpClient) { }
+
+  getWordDef(word:string) {
+    return new Promise(resolve => {
+      this.http.get(this.wordAPIUrl+word,httpOptions).subscribe(data => {
+        
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+
 }
